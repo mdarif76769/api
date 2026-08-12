ca39c4c95d6e42b242d0efa40e1cbe91c0a50b07
RS5Hack_ARIF <146631213+mdarif76769@users.noreply.github.com>
2026-08-12 07:31:09 +0600

    Update update_url in Update_V1.js

--- Update_V1.js
+++ Update_V1.js
@@ -2,5 +2,5 @@
   "version": "1.1",
   "title": "AI Chat",
   "subtitle": "⚡ Funciones Pro Desbloqueadas\n\n✅ Respuestas sin límites e instantáneas\n✅ Modelo GPT-4o e Claude 3.5 fully unlocked\n✅ Historial en la nube corregido\n\n¡Gracias por usar este Mod! 🚀",
-  "update_url": "https://t.me"
+  "update_url": "https://t.me/RS5ARIF"
 }
