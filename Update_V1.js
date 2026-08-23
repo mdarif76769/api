05e5c0d709140bff6d697f8411aa03dc7835ac17
RS5Hack_ARIF <146631213+mdarif76769@users.noreply.github.com>
2026-08-23 21:13:39 +0600

    Delete system update

--- Update_V1.js
+++ Update_V1.js
@@ -1,11 +1,5 @@
 [
     {
-  "version": " 4",
-  "title": "AI Chat",
-  "subtitle": "⚡ Funciones Pro Desbloqueadas\\n\\n✅ Respuestas sin límites e instantáneas\\n✅ Modelo GPT-4o e Claude 3.5 fully unlocked\\n✅ Historial en la nube corregido\\n\\n¡Gracias por usar este Mod! 🚀",
-  "update_url": "https://t.me/RS5ARIF"
-},
-    {
         "credit": "MR. NoOB",
         "show_dialog": true,
         "title": "🚀 Update Available",
