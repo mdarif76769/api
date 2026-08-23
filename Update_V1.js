4c6ec65462ee57103633947c69ec73bccc631e9c
RS5Hack_ARIF <146631213+mdarif76769@users.noreply.github.com>
2026-08-23 20:40:09 +0600

    Update version details in Update_V1.js

--- Update_V1.js
+++ Update_V1.js
@@ -1,8 +1,8 @@
 [
     {
-        "version": "",
-        "title": "",
-        "subtitle": "",
-        "update_url": ""
+        "version": "4",
+        "title": "4.1",
+        "subtitle": "xugxgxiggi",
+        "update_url": "dtidiyiyiyy"
     }
 ]
