efc6eea144f7857d4dd37c239e0cbcfd7cbf9db2
RS5Hack_ARIF <146631213+mdarif76769@users.noreply.github.com>
2026-08-23 21:27:46 +0600

    Add version 2.0 details to Update_V1.js
    Updated version details and added new subtitle for version 2.0.

--- Update_V1.js
+++ Update_V1.js
@@ -1,5 +1,12 @@
 [
     {
+  "version": "2.0",
+  "title": "Dialogo",
+  "subtitle": "⚡ Funciones Pro Desbloqueadas\n\n✅ Respuestas sin límites e instantáneas\n✅ Modelo GPT-4o e Claude 3.5 fully unlocked\n✅ Historial en la nube corregido\n\n¡Gracias por usar este Mod! 🚀",
+  "update_url": "https://t.me/canalmoded3",
+  "show_close_button": true
+},
+    {
         "version": "4.0",
         "title": "Chat-gpt",
         "subtitle": "New apdate",
