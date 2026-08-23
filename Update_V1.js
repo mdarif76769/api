c21149985b5a23a887f755bf7f787cc49b106140
RS5Hack_ARIF <146631213+mdarif76769@users.noreply.github.com>
2026-08-23 16:15:33 +0600

    Add system update info

--- Update_V1.js
+++ Update_V1.js
@@ -1,6 +1,14 @@
-{
-    "version": "",
-    "title": "",
-    "subtitle": "",
-    "update_url": ""
-}
+[
+    {
+        "version": "4.0.1",
+        "title": "Onek kiso",
+        "subtitle": "Higise/n/n/-+_(+-&;(\"865*85",
+        "update_url": "Kiggmkn"
+    },
+    {
+        "version": "",
+        "title": "",
+        "subtitle": "",
+        "update_url": ""
+    }
+]
