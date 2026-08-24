85b4c1f2b5d507836b51e54514145a185c665ca7
RS5Hack_ARIF <146631213+mdarif76769@users.noreply.github.com>
2026-08-25 00:46:33 +0600

    Remove outdated version details and retain current version

--- Update_V1.js
+++ Update_V1.js
@@ -1,25 +1,9 @@
 [
     {
-  "version": "2.0",
-  "title": "Dialogo",
-  "subtitle": "⚡ Funciones Pro Desbloqueadas\n\n✅ Respuestas sin límites e instantáneas\n✅ Modelo GPT-4o e Claude 3.5 fully unlocked\n✅ Historial en la nube corregido\n\n¡Gracias por usar este Mod! 🚀",
-  "update_url": "https://t.me/canalmoded3",
-  "show_close_button": true
-},
-    {
-        "version": "4.0",
-        "title": "Chat-gpt",
-        "subtitle": "New apdate",
-        "update_url": "https:// t.me/ RS5ARIF"
-    },
-    {
-        "credit": "MR. NoOB",
-        "show_dialog": true,
-        "title": "🚀 Update Available",
-        "subtitle": "✨ New Version Release ✨",
-        "whatsnew": "• Improved app performance\n• Fixed major bugs\n• Enhanced security layers\n• Polished UI with smooth animations ",
-        "btn_update": "Update Now",
-        "btn_exit": "Exit",
-        "link": "https://t.me/EpicModsChannel"
+        "version": "2.0",
+        "title": "Dialogo",
+        "subtitle": "⚡ Funciones Pro Desbloqueadas\n\n✅ Respuestas sin límites e instantáneas\n✅ Modelo GPT-4o e Claude 3.5 fully unlocked\n✅ Historial en la nube corregido\n\n¡Gracias por usar este Mod! 🚀",
+        "update_url": "https://t.me/canalmoded3",
+        "show_close_button": true
     }
 ]
